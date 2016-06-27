a76bcc5ae2136133fdfbd08d3184acdd690b2c6f
bondgamesbond <gevendary@gmail.com>
2016-06-27 15:09:07 +0300

    Initial Commit

---
+++ Necro/Assets/Scripts/AIController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AIController : MonoBehaviour {
+
+    public List<GameObject> Bots;
+    public GameObject Bot;
+
+ 
+
+    // Use this for initialization
+    void Start () {
+
+        for (int i =0; i<Random.Range(1,15); i++)
+        {
+             Bots.Add((Instantiate(Bot, new Vector3(0, 0, 0),Quaternion.identity)) as GameObject);
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+      
+
+    }
+}
---
+++ Necro/Assets/Scripts/BotController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BotController : MonoBehaviour {
+    private Vector2 way;
+    public List<GameObject> Skeletons;
+    public GameObject Skeleton;
+    private int skeletonCount;
+
+    // Use this for initialization
+    void Start () {
+        skeletonCount = Random.Range(1, 15);
+        way = new Vector2(Random.Range(-6, 6), Random.Range(-6, 6));
+        for (int i = 0; i < skeletonCount; i++)
+        {
+            Skeletons.Add((Instantiate(Skeleton, new Vector2(transform.position.x, transform.position.y), Quaternion.identity)) as GameObject);
+           // Skeletons[i].transform.parent = transform;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+        for (int i = 0; i < skeletonCount; i++)
+        {
+           Skeletons[i].transform.position = Vector2.MoveTowards(Skeletons[i].transform.position, transform.position,  1.8f*Time.deltaTime);
+            if ((transform.position.x == way.x && transform.position.y == way.y) )
+            {
+                way = new Vector2(Random.Range(-10, 10), Random.Range(-10, 10));
+            }
+        }
+        transform.position = Vector2.MoveTowards(transform.position, way, 1.8f * Time.deltaTime);
+
+
+    }
+}
